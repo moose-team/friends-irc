8fe35f52cb8b1cb06e18199ebbff938ef2f3b0c1
ekmartin <mail@ekmartin.no>
2015-05-02 21:43:32 +0200

    Use feross/standard

---
+++ index.js
@@ -0,0 +1,22 @@
+#!/usr/bin/env node
+
+var program = require('commander')
+var checkEnv = require('check-env')
+var createBots = require('./lib/helpers').createBots
+var logger = require('winston')
+
+if (process.env.NODE_ENV === 'development') {
+  logger.level = 'debug'
+}
+
+program
+  .option('-c, --config <path>',
+    'Sets the path to the config file, otherwise read from the env variable CONFIG_FILE.'
+  )
+  .parse(process.argv)
+
+// If no config option is given, try to use the env variable:
+if (!program.config) checkEnv(['CONFIG_FILE'])
+else process.env.CONFIG_FILE = program.config
+
+createBots()
